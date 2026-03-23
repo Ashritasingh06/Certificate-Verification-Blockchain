e422161e952d609e24400b2a914848fcb43182b4
AshritaSingh <ashritasingh1212@gmail.com>
2026-03-23 09:14:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function verify(){
 var id = document.getElementById("certId").value.trim();
 var hash = document.getElementById("certHash").value.trim();
 
-// VALID CERTIFICATE (hardcoded for demo)
+console.log(id);
+console.log(hash);
+
 if(id === "9763872" && hash === "b506eb2a95f480fad3ee20d97496b9bdbf1c51f50391bba330b2974dcc349ab6")
 {
 document.getElementById("result").innerHTML = "Certificate Verified ✅";
