df7e46f1f201253261dcd53d182af57df481dbf4
AshritaSingh <ashritasingh1212@gmail.com>
2026-03-23 09:11:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function verify(){
 
-var id=document.getElementById("certId").value;
-var hash=document.getElementById("certHash").value;
+var id = document.getElementById("certId").value.trim();
+var hash = document.getElementById("certHash").value.trim();
 
-if(id=="187065eb3d354746b18827a267d56a50" && hash=="b506eb2a95f480fad3ee20d97496b9bdbf1c51f50391bba330b2974dcc349ab6")
+// VALID CERTIFICATE (hardcoded for demo)
+if(id === "9763872" && hash === "b506eb2a95f480fad3ee20d97496b9bdbf1c51f50391bba330b2974dcc349ab6")
 {
-document.getElementById("result").innerHTML="Certificate Verified ✅";
+document.getElementById("result").innerHTML = "Certificate Verified ✅";
 }
 else
 {
-document.getElementById("result").innerHTML="Invalid Certificate ❌";
+document.getElementById("result").innerHTML = "Invalid Certificate ❌";
 }
 
 }
